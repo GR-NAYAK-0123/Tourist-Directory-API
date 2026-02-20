d07b04bb81eac3f44f3f20be1ecffe1387a0b99a
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-02-20 17:46:52 +0530

    Created a Spring Boot Rest Application where I used maintains the tourist info

---
+++ src/main/java/com/tourist/Tourist_Register/TouristRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.tourist.Tourist_Register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TouristRegisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tourist/Tourist_Register/TouristRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tourist.Tourist_Register;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouristRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
