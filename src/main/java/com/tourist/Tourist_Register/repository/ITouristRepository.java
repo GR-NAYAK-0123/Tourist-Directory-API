4bb117c4254486936f30a7f6cdac233f2b059fcb
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-02-20 18:04:25 +0530

    Created the Entity Tourist class and added all the properties in application.properties

--- src/main/java/com/tourist/Tourist_Register/repository/ITouristRepository.java
+++ src/main/java/com/tourist/Tourist_Register/repository/ITouristRepository.java
@@ -1,5 +1,6 @@
 package com.tourist.Tourist_Register.repository;
 
+import com.tourist.Tourist_Register.entity.Tourist;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ITouristRepository extends JpaRepository<Tourist, Integer> {
